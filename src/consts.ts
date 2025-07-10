47675d3dda1a2ede98a194f49218fc40f13c3f0e
kyogesha <yogesha.k@314ecorp.com>
2025-07-10 07:59:35 +0530

    add astro nano theme

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,44 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
+import type { Site, Metadata, Socials } from "@types";
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE: Site = {
+  NAME: "Yogesh Kaddipudi",
+  EMAIL: "yogesh.kaddipudi@example.com",
+  NUM_POSTS_ON_HOMEPAGE: 3,
+  NUM_WORKS_ON_HOMEPAGE: 2,
+  NUM_PROJECTS_ON_HOMEPAGE: 3,
+};
+
+export const HOME: Metadata = {
+  TITLE: "Home",
+  DESCRIPTION: "A minimal and lightweight blog and portfolio.",
+};
+
+export const BLOG: Metadata = {
+  TITLE: "Blog",
+  DESCRIPTION: "A collection of articles on topics I am passionate about.",
+};
+
+export const WORK: Metadata = {
+  TITLE: "Work",
+  DESCRIPTION: "Where I have worked and what I have done.",
+};
+
+export const PROJECTS: Metadata = {
+  TITLE: "Projects",
+  DESCRIPTION: "A collection of my projects, with links to repositories and demos.",
+};
+
+export const SOCIALS: Socials = [
+  { 
+    NAME: "twitter-x",
+    HREF: "https://twitter.com/your-twitter",
+  },
+  { 
+    NAME: "github",
+    HREF: "https://github.com/your-github"
+  },
+  { 
+    NAME: "linkedin",
+    HREF: "https://www.linkedin.com/in/your-linkedin",
+  }
+];
