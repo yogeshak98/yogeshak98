030f9989555db8feb3b0c55343a2864052823e81
kyogesha <yogesha.k@314ecorp.com>
2025-07-14 21:18:58 +0530

    update domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://yogeshkaddipudi.com",
+  site: "https://yogeshk.pages.dev",
   integrations: [mdx(), sitemap(), tailwind()],
 });
